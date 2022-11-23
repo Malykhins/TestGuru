bdb042930a47e7cdb9cb0ebb6492692c857a2588
Malykhins <malykhins@mail.ru>
2022-11-23 22:41:58 +0300

    create class method sort_test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
+  def self.sort_tests(name_of_catgory)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: name_of_catgory })
+      .order(title: :desc)
+      .pluck(:title)
+  end
 end
