2fef9d7f01324516eb6b75505e85f57dd30fc8a0
Malykhins <malykhins@mail.ru>
2022-11-23 22:52:26 +0300

    сorrecting an error in the method name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  def self.sort_tests(name_of_catgory)
+  def self.sort_tests(name_of_category)
     joins('JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: name_of_catgory })
+      .where(categories: { title: name_of_category })
       .order(title: :desc)
       .pluck(:title)
   end
