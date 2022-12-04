40e618087953f0629a01cfd27158dc62392d870e
Malykhins <malykhins@mail.ru>
2022-12-04 16:14:57 +0300

    adding scope methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,11 +8,16 @@ class Test < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users, dependent: :nullify
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :sort_categories, lambda { |name_of_category|
+                            where(categories: { title: name_of_category })
+                              .order(title: :desc)
+                              .pluck(:title)
+                          }
 
   def self.sort_tests(name_of_category)
-    joins(:category)
-      .where(categories: { title: name_of_category })
-      .order(title: :desc)
-      .pluck(:title)
+    joins(:category).sort_categories(name_of_category)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users, dependent: :destroy
   has_many :my_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
 
+  scope :test_level, ->(level) { where(level: level) }
+
   def view_user_tests(level)
-    tests.where(level: level)
+    tests.test_level(level)
   end
 end
