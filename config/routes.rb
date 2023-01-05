83b28c8920beff6fbb12183f69405107a0ad49ef
Malykhins <malykhins@mail.ru>
2023-01-05 18:51:21 +0300

    added a controller of answers with views, added paths for the answers resource, changed the file view of the question (all answers are displayed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :answers
   resources :tests, shallow: true do
-    resources :questions, except: :index
+    resources :questions, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
