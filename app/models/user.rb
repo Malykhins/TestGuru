abd25bc859bba6d83fbccad908f249db161cfb6a
Malykhins <malykhins@mail.ru>
2023-01-15 17:37:45 +0300

    create test passage entity, add routes, change test views, delete tests_users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :tests_users, dependent: :destroy
-  has_many :tests, through: :tests_users
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
   has_many :my_tests, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
 
   validates :email, presence: true
@@ -11,5 +11,9 @@ class User < ApplicationRecord
 
   def view_user_tests(level)
     tests.test_level(level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ users = User.create!([
 
 tests = Test.create!([
                        { title: 'Ruby', level: 1, category: categories[0], author: users[0] },
-                       { title: 'Rails', level: 2, category: categories[1], author: users[1] },
+                       { title: 'Rails', level: 2, category: categories[0], author: users[1] },
                        { title: 'HTML', level: 1, category: categories[1], author: users[2] }
                      ])
 
@@ -27,22 +27,16 @@ Answer.create!([
                  { body: 'Yukihiro Matsumoto', correct: true, question: questions[0] },
                  { body: 'Rambo', correct: false, question: questions[0] },
                  { body: 'Rubanov Vasily', correct: false, question: questions[0] },
-                 { body: 'Gemfile.lock', correct: false, question: questions[1] },
-                 { body: 'gems.rb', correct: false, question: questions[1] },
-                 { body: 'Gemfile', correct: true, question: questions[1] },
-                 { body: '**', correct: false, question: questions[2] },
-                 { body: '<>', correct: true, question: questions[2] },
-                 { body: '||', correct: false, question: questions[2] }
+                 { body: 'True', correct: false, question: questions[1] },
+                 { body: 'False', correct: true, question: questions[1] },
+                 { body: 'Gemfile.lock', correct: false, question: questions[2] },
+                 { body: 'gems.rb', correct: false, question: questions[2] },
+                 { body: 'Gemfile', correct: true, question: questions[2] },
+                 { body: '**', correct: false, question: questions[3] },
+                 { body: '<>', correct: true, question: questions[3] },
+                 { body: '||', correct: false, question: questions[3] }
                ])
 
-TestsUser.create!([
-                    { user: users[0], test: tests[0] },
-                    { user: users[0], test: tests[1] },
-                    { user: users[0], test: tests[2] },
-                    { user: users[1], test: tests[1] },
-                    { user: users[1], test: tests[2] },
-                    { user: users[2], test: tests[0] }
-                  ])
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
