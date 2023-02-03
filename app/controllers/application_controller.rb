c71ab308d9b4f22a82ccea0d988ad2a9776163f4
Malykhins <malykhins@mail.ru>
2023-02-03 10:38:54 +0300

    set the default locale, added a method to set the locale based on the url and the default_url_options method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
   def after_sign_in_path_for(user)
     if user.is_a?(Admin)
@@ -8,4 +9,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def default_url_options
+    { lang: I18n.locale } unless params[:lang] == :ru
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 end
