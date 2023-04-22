69e9088e4cd68ac372d68d10a5486f5aa4ff17ec
Malykhins <malykhins@mail.ru>
2023-04-22 20:08:39 +0300

    added default_url_options to prodaction.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'https://testguru-7yia.onrender.com' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.mail.ru',
     port: 465,
